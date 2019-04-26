9c5d02afdfd7de4660acda13c486394aa6409a8d
chichra <vishakhachichra@gmail.com>
2019-04-26 17:17:35 +0530

    added dfs to graph

--- src/graph/index.js
+++ src/graph/index.js
@@ -27,17 +27,17 @@ class Graph{
     }
 
     removeEdge(vertexName, edgeName){
-        
+
         if(this.Adjlist.size === 0)
         {
             return "Empty Graph";
         }
-        
+
         if(!this.Adjlist.get(vertexName))
         {
             return "Vertex does not exist in the Graph";
         }
-        
+
         let location = this.Adjlist.get(vertexName).indexOf(edgeName);
 
         if(location === -1){
@@ -46,11 +46,11 @@ class Graph{
         else{
             return this.Adjlist.get(vertexName).splice(location, 1).toString();
         }
-        
+
     }
 
     bfs(startNode){
-        
+
         if(!Array.from(this.Adjlist.keys()).includes(startNode)){
             return "Node doesn't exist in the Graph";
         }
@@ -68,15 +68,15 @@ class Graph{
         while(queue.length > 0){
             let getQueueElement = queue.shift();
             output.push(getQueueElement);
-            
+
             let getList =  this.Adjlist.get(getQueueElement);
 
             for(let i = 0; i < getList.length; i++){
-                
+
                 let neighbour = getList[i];
-                
+
                 if(!visited[neighbour]){
-                    
+
                     visited[neighbour] = true;
                     queue.push(neighbour);
                 }
@@ -84,6 +84,49 @@ class Graph{
         }
         return output;
     }
+
+
+dfs(startNode)
+{
+
+	if(!Array.from(this.Adjlist.keys()).includes(startNode))
+	{
+            return "Node doesn't exist in the Graph";
+	}
+	let visited = [];
+	let output = [];
+
+	for (let i = 0; i < this.noOfVertices; i++){
+		visited.push(false);
+	}
+
+	this.dfsRec(startNode, visited, output);
+
+	return output;
+}
+
+dfsRec(vert, visited, output)
+{
+	visited[vert] = true;
+	output.push(vert);
+
+	var getNeighbours = this.Adjlist.get(vert);
+
+	if(getNeighbours.length > 0)
+	{
+
+		for (let i=0; i<getNeighbours.length; i++) {
+		var getElem = getNeighbours[i];
+		if (!visited[getElem]){
+			this.dfsRec(getElem, visited, output);
+		}
+
+	}
+	}
+
+}
+
+
 }
 
 module.exports = Graph;
